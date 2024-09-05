7f54374a5dc576b7f9949b31993f28c6426a249c
Sangamithra Nalam <laxmipriyanalam1212@gmail.com>
2024-09-05 12:07:59 +0530

    React

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-//const cors=require('cors')
+const cors=require('cors')
 const studentRouter = require('./routes/students')
 
 
 //const url = 'mongodb://127.0.0.1:27017/CBIT'
 //const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit3?replicaSet=m101';
-const url ='mongodb+srv://<username>:<password>@merncrud1.1d54v.mongodb.net/?retryWrites=true&w=majority&appName=MERNcrud1'
+const url ='mongodb+srv://laxmipriyanalam1212:nalam12@merncrud1.1d54v.mongodb.net/?retryWrites=true&w=majority&appName=MERNcrud1'
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
@@ -16,7 +16,7 @@ con.on('open', () =>
 {
 console.log('connected...')
 })
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 app.use('/students',studentRouter)
